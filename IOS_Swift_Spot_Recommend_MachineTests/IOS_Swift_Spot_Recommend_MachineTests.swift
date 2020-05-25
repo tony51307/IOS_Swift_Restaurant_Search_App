6515bc8631d5936e9594c52e9b5a7cf5396f87cb
tony51307 <59693085+tony51307@users.noreply.github.com>
2020-05-25 20:19:03 +0800

    Initial Commit

---
+++ IOS_Swift_Spot_Recommend_MachineTests/IOS_Swift_Spot_Recommend_MachineTests.swift
@@ -0,0 +1,34 @@
+//
+//  IOS_Swift_Spot_Recommend_MachineTests.swift
+//  IOS_Swift_Spot_Recommend_MachineTests
+//
+//  Created by Tony Lee on 2020/5/25.
+//  Copyright © 2020 Tony Li. All rights reserved.
+//
+
+import XCTest
+@testable import IOS_Swift_Spot_Recommend_Machine
+
+class IOS_Swift_Spot_Recommend_MachineTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
